6b8659ee4631d546db6e1d5a24b118922a7bb716
tkpatel2285 <tkpatel2285@gmail.com>
2019-03-03 11:54:37 +0000

    making changes

--- Cucumber2/src/test/java/Gerkin/RunTest.java
+++ Cucumber2/src/test/java/Gerkin/RunTest.java
@@ -7,10 +7,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features=".",tags = "@all",format = {"pretty","html:target/cucumber-reports"})
+@CucumberOptions(features=".",tags = "@priceHighToLow",format = {"pretty","html:target/cucumber-reports"})
 
 public class RunTest  {
-
+// hi i did change
 }
 
 
